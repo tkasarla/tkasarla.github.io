ddc90e575bb5cd29af9c447c26accc205cb3cf2b
Tejaswi Kasarla <11936990+tkasarla@users.noreply.github.com>
2019-01-03 16:43:08 +0530

    Update common.js

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     $('abstract').click(function() {
-        $(this).parent().parent().find("abstract hidden").toggleClass('open');
+        $(find("abstract hidden").toggleClass('open'));
     });
     $('bibtex').click(function() {
         $(this).parent().parent().find(".bibtex.hidden").toggleClass('open');
